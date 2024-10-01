f1958e124e56073013a4f0e6f4d034636464b243
Kayum Ali <sisirfahimstudent173@gmail.com>
2024-10-01 15:07:46 +0600

    mongoDB succeccfully connected on databse

--- index.js
+++ index.js
@@ -4,16 +4,47 @@ require('dotenv').config()
 const port = process.env.PORT || 5000;
 const app = express();
 
+// const corsOptions = {
+//     origin: ['http://localhost:5173', 'http://localhost:5174'],
+//     credentials: true,
+//     optionsSuccessStatus: 200 
+// }
 app.use(cors());
 app.use(express.json());
 
+// mongodb connection
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dangeag.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    
+  }
+}
+run().catch(console.dir);
+
+
 
 // Mock data
-app.get('/', async(req, res)=>{
-    console.log('SoloSphere server is running')
+app.get('/', (req, res)=>{
+    res.send('server in running')
 })
 
 app.listen(port, ()=>{
     console.log(`Server running on port ${port}`)
- ;
 })
